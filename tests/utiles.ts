97c6a2cdd8feff74852eaef6b2250fa3969f8504
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-09 00:00:02 +0100

    Add getStateAddress util function

--- tests/utiles.ts
+++ tests/utiles.ts
@@ -1,6 +1,8 @@
+import { utils } from '@project-serum/anchor'
 import { PublicKey } from "@solana/web3.js"
 
 export const SEED = 'EVENTSNAP'
+export const STATE_SEED = 'STATE'
 
 export const getProgramAuthority = async (programId: PublicKey) => {
     const [programAuthority, nonce] = await PublicKey.findProgramAddress(
@@ -11,5 +13,17 @@ export const getProgramAuthority = async (programId: PublicKey) => {
     return {
         programAuthority,
         nonce
+    }
+}
+
+export const getStateAddress = async (programId: PublicKey) => {
+    const [address, bump] = await PublicKey.findProgramAddress(
+        [Buffer.from(utils.bytes.utf8.encode(STATE_SEED))],
+        programId
+    )
+
+    return {
+        address,
+        bump
     }
 }
