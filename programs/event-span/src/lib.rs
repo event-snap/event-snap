ee48e475871f257b9d7e59efcd2d92374c9791b2
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-09 00:05:35 +0100

    Add initialize_state instruction

--- programs/event-span/src/lib.rs
+++ programs/event-span/src/lib.rs
@@ -1,18 +1,45 @@
+use crate::structs::state::State;
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 mod macros;
 pub mod structs;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// TODO: use this constants
+const SEED: &str = "EVENTSPAN";
+const STATE_SEED: &str = "STATE";
 
 #[program]
 pub mod event_span {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, nonce: u8) -> Result<()> {
+        let state = &mut ctx.accounts.state.load_init()?;
+        **state = State {
+            nonce,
+            bump: *ctx.bumps.get("state").unwrap(),
+            program_authority: *ctx.accounts.program_authority.key,
+            eventsnap_admin: *ctx.accounts.admin.key,
+            // event_buffer
+        };
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction( nonce: u8)]
+pub struct Initialize<'info> {
+    #[account(init, seeds = [b"STATE".as_ref()], bump, space = State::LEN, payer = admin)]
+    pub state: AccountLoader<'info, State>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    /// CHECK: safe as seed checked
+    #[account(seeds = [b"EVENTSPAN".as_ref()], bump = nonce)]
+    pub program_authority: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    /// CHECK: safe as constant
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+}
--- programs/event-span/src/structs/state.rs
+++ programs/event-span/src/structs/state.rs
@@ -1,11 +1,15 @@
 use anchor_lang::prelude::*;
 
+use crate::size;
+
 #[account(zero_copy)]
 #[repr(packed)]
 #[derive(PartialEq, Default, Debug)]
 pub struct State {
-    pub eventsnap_admin: Pubkey,
     pub nonce: u8,
-    pub program_authority: Pubkey,
     pub bump: u8,
+    pub program_authority: Pubkey,
+    // pub event_buffer: Pubkey,
+    pub eventsnap_admin: Pubkey,
 }
+size!(State);
