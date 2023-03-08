16b756cb4d0c52b545e8c907055fb01267209c83
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-08 22:31:26 +0100

    Init repo

---
+++ programs/event-span/src/lib.rs
@@ -0,0 +1,18 @@
+use anchor_lang::prelude::*;
+
+// pub mod structs;
+// mod macros;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod event_span {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
