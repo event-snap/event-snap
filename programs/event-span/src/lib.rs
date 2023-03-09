95e33942a07665c9720bcc623f4344044fcc6b87
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-09 18:18:14 +0100

    Add event_buffer

--- programs/event-span/src/lib.rs
+++ programs/event-span/src/lib.rs
@@ -6,8 +6,10 @@ mod macros;
 pub mod structs;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+// TODO: change type from &str to &[u8]
 const AUTHORITY_SEED: &str = "EVENTSNAP";
 const STATE_SEED: &str = "STATE";
+const EVNET_BUFFER: &str = "EVENT_BUFFER";
 
 #[program]
 pub mod event_span {
@@ -15,17 +17,27 @@ pub mod event_span {
 
     pub fn initialize(ctx: Context<Initialize>, nonce: u8) -> Result<()> {
         let state = &mut ctx.accounts.state.load_init()?;
+        let event_buffer = *ctx.accounts.event_buffer.to_account_info().key;
+
         **state = State {
             nonce,
             bump: *ctx.bumps.get("state").unwrap(),
             program_authority: *ctx.accounts.program_authority.key,
             eventsnap_admin: *ctx.accounts.admin.key,
-            // event_buffer
+            event_buffer,
         };
 
         Ok(())
     }
+
+    // pub fn withdraw_event_buffer(ctx: Context<WithdrawEventBuffer>) -> Result<()> {
+    //     Ok(())
+    // }
 }
+
+// TODO: rent in not required
+// TODO: passing program_authority is not required
+// TODO: I can use Account with empty strcut insted of UncheckedAccount
 
 #[derive(Accounts)]
 #[instruction( nonce: u8)]
@@ -37,8 +49,38 @@ pub struct Initialize<'info> {
     /// CHECK: safe as seed checked
     #[account(seeds = [AUTHORITY_SEED.as_bytes().as_ref()], bump = nonce)]
     pub program_authority: AccountInfo<'info>,
+    /// CHECK: safe as seed checked
+    #[account(
+        init,
+        payer = admin,
+        space = 0,
+        seeds = [EVNET_BUFFER.as_bytes()],
+        bump,
+    )]
+    pub event_buffer: UncheckedAccount<'info>,
     pub rent: Sysvar<'info, Rent>,
     /// CHECK: safe as constant
     #[account(address = system_program::ID)]
     pub system_program: AccountInfo<'info>,
 }
+
+// #[derive(Accounts)]
+// pub struct WithdrawEventBuffer<'info> {
+//     // #[account(init, seeds = [STATE_SEED.as_bytes().as_ref()], bump, space = State::LEN, payer = admin)]
+//     // pub state: AccountLoader<'info, State>,
+//     // #[account(mut)]
+//     // pub admin: Signer<'info>,
+//     // /// CHECK: safe as seed checked
+//     // #[account(seeds = [AUTHORITY_SEED.as_bytes().as_ref()], bump = nonce)]
+//     // pub program_authority: AccountInfo<'info>,
+
+//     // // #[account(init,
+//     // //     token::authority = authority,
+//     // //     payer = payer,
+//     // // )]
+//     // // pub event_buffer: Account<'info, >,
+//     // pub rent: Sysvar<'info, Rent>,
+//     // /// CHECK: safe as constant
+//     // #[account(address = system_program::ID)]
+//     // pub system_program: AccountInfo<'info>,
+// }
--- programs/event-span/src/structs/state.rs
+++ programs/event-span/src/structs/state.rs
@@ -9,7 +9,7 @@ pub struct State {
     pub nonce: u8,
     pub bump: u8,
     pub program_authority: Pubkey,
-    // pub event_buffer: Pubkey,
+    pub event_buffer: Pubkey,
     pub eventsnap_admin: Pubkey,
 }
 size!(State);
