5fe48f8c7ecc125fcdbfcfcee25305137d9569c0
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-10 15:07:48 +0100

    Testing deposit event buffer instruction

--- tests/event-span.spec.ts
+++ tests/event-span.spec.ts
@@ -1,5 +1,5 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { BN, Program } from "@project-serum/anchor";
 import { EventSpan } from "../target/types/event_span";
 import { Keypair, SystemProgram, SYSVAR_RENT_PUBKEY, Transaction } from '@solana/web3.js'
 import { getEventBufferAddress, getProgramAuthority, getStateAddress, signAndSend, sleep } from "./utiles";
@@ -10,10 +10,11 @@ describe("event-span", () => {
   const connection = provider.connection
   const program = anchor.workspace.EventSpan as Program<EventSpan>;
   const admin = Keypair.generate()
+  const solAmount = 1e9
   anchor.setProvider(provider);
 
   before(async () => {
-    await connection.requestAirdrop(admin.publicKey, 1e9)
+    await connection.requestAirdrop(admin.publicKey, solAmount)
     await sleep(500)
   })
 
@@ -22,7 +23,7 @@ describe("event-span", () => {
     const { state } = await getStateAddress(program.programId)
     const { eventBuffer } = await getEventBufferAddress(program.programId)
 
-    const ix = program.instruction.initialize(nonce, {
+    const initIx = program.instruction.initialize(nonce, {
       accounts: {
         state,
         eventBuffer,
@@ -32,7 +33,17 @@ describe("event-span", () => {
         systemProgram: SystemProgram.programId
       }
     })
-    const tx = new Transaction().add(ix)
-    await signAndSend(tx, [admin], connection)
+    await signAndSend(new Transaction().add(initIx), [admin], connection)
+
+    const depositIx = program.instruction.depositEventBuffer(new BN(solAmount / 10), {
+      accounts: {
+        state,
+        depositor: admin.publicKey,
+        eventBuffer,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: SystemProgram.programId
+      }
+    })
+    await signAndSend(new Transaction().add(depositIx), [admin], connection)
   });
 });
