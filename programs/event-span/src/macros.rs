90e446548bf3154441f15f8eb549be7fab9a2d45
wojciech-cichocki <cichockiwojciech97@gmail.com>
2023-03-15 01:44:38 +0100

    Add array_ref macro

--- programs/event-span/src/macros.rs
+++ programs/event-span/src/macros.rs
@@ -13,3 +13,21 @@ macro_rules! size {
         }
     };
 }
+
+#[macro_export]
+macro_rules! array_ref {
+    ($arr:expr, $offset:expr, $len:expr) => {{
+        {
+            #[inline]
+            unsafe fn as_array<T>(slice: &[T]) -> &[T; $len] {
+                &*(slice.as_ptr() as *const [_; $len])
+            }
+            let offset = $offset;
+            let slice = &$arr[offset..offset + $len];
+            #[allow(unused_unsafe)]
+            unsafe {
+                as_array(slice)
+            }
+        }
+    }};
+}
